c63fb8d90be2c6a7a1d1bb647fcad7c906054d13
Ruchika <rus-techie@users.noreply.github.com>
2019-08-10 12:58:36 -0700

    reverseString method added

--- Demo.java
+++ Demo.java
@@ -1,6 +1,19 @@
 public class DemoJava {
 
+	public static reverseString(String S){
+		char ch[] = S.toCharArray();
+		int len = S.length();
+		char reverse[] = char[len];
+		for (int i=0, j=len-1;j>=0; i++, j--)
+		{
+			reverse[i] = ch[j];	
+		}
+	System.out.println("");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Testing Java");
+		String s = "I am Great";
+		reverseString(S);
 	}
 }
